fc550fd85d2451438fca7258607364a722a94b98
Manuel Mendes <manuelmendes572@gmail.com>
2025-09-03 10:25:14 -0500

    Add files via upload

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("We workin', yo!");
-    alert("Hey!")
+    console.log("Starting up!");
+
+    document.getElementById('btnClicky').addEventListener('click', btnClicky);
+    //alert("Hey!")
   });
 
-console.log("We workin', yo!")
+function btnClicky () {
+  console.log('in btnClicky event');
+  document.getElementById('btnClicky').style.transform='rotate(15deg)';
+  console.log('out of btnClicky event');
+    //.querySelectorAll('.yourClassName')
+    //.forEach(el => { el.style.transform = 'rotate(15deg)'; });
+}
 
+  
