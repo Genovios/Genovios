a34fe98073b6f66110a3babf41a92599c229e698
Manuel Mendes <manuelmendes572@gmail.com>
2025-09-15 17:39:54 -0500

    Add files via upload

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,12 @@
+function btnClicky () {
+  console.log('in btnClicky event');
+  //document.getElementById('btnClicky').style.transform='rotate(15deg)';
+  document.getElementById('btnClicky').classList.add('buttonColor');
+  console.log('out of btnClicky event');
+    //.querySelectorAll('.yourClassName')
+    //.forEach(el => { el.style.transform = 'rotate(15deg)'; });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Starting up!");
 
@@ -5,12 +14,5 @@ document.addEventListener("DOMContentLoaded", () => {
     //alert("Hey!")
   });
 
-function btnClicky () {
-  console.log('in btnClicky event');
-  document.getElementById('btnClicky').style.transform='rotate(15deg)';
-  console.log('out of btnClicky event');
-    //.querySelectorAll('.yourClassName')
-    //.forEach(el => { el.style.transform = 'rotate(15deg)'; });
-}
 
-  
+$( document ).ready(function() {});
